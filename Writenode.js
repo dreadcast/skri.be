a9004f5149d8e7c4d0bb68775a0707ebcae90517
Raphaël Gully <raphael.gully@kubity.com>
2015-11-04 16:26:06 +0100

    add medias from articles service

--- Writenode.js
+++ Writenode.js
@@ -12,6 +12,8 @@ import assets from './services/assets';
 
 let services = {};
 
+// import fs from 'fs-extra';
+
 var timer = {};
 
 timestamp('start');
@@ -20,12 +22,18 @@ function timestamp(label){
 	timer[label] = Date.now();
 }
 
+var ready = false;
+export function isReady(){
+	return ready;
+}
+
 export function addService(name, service, ...args){
 	if(services[name]){
 		return Bluebird.resolve(services[name]);
 	}
 
 	args.unshift({
+		isReady,
 		addService,
 		getService,
 		timestamp
@@ -46,13 +54,16 @@ export function getService(name){
 export function dev(pathToTheme){
 	return addService('conf', conf, pathToTheme)
 		.then(() => addService('watcher', watcher))
-		.then(() => addService('articles', articles))
 		.then(() => addService('medias', medias))
+		.then(() => addService('articles', articles))
 		.then(() => addService('assets', assets))
 		.then(() => addService('views', views))
 		.then(() => addService('server', server))
 		.then(() => {
 			timestamp('end');
 			console.info(timer, (timer.end - timer.start) + 'ms');
+			ready = true;
+
+			// return fs.outputJson(Path.join(getService('conf').pathToBlog, 'data.json'), getService('articles').toJSON());
 		});
 }
