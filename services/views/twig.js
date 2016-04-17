12c05950c94d2cd37c7016915a62178f8491c84b
Raphaël Gully <concept@dreadcast.com>
2016-04-18 00:00:03 +0200

    cleanup

--- services/views/twig.js
+++
@@ -1,9 +0,0 @@
-import swig from 'swig';
-
-export default function precompile(pathToTemplate){
-	var render = swig.compileFile(pathToTemplate, {
-		filename: pathToTemplate
-	});
-
-	return Promise.resolve(render);
-}
