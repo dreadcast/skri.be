648ad96317d1cc51e79f94c5b30f6aebc4def283
Raphaël Gully <concept@dreadcast.com>
2016-05-09 23:42:11 +0200

    play with Jimp

--- src/modules/media/LocalMediaActions.js
+++ src/modules/media/LocalMediaActions.js
@@ -1,5 +1,7 @@
 import { join } from 'path';
 import { merge } from 'ramda';
+import Jimp from 'jimp';
+import Bluebird from 'bluebird';
 import { getImageInfo } from './LocalMediaClient';
 import { UPDATE_MEDIA, getRatio } from './MediaActions';
 import logger from './../../util/logger';
@@ -18,6 +20,7 @@ export function getLocalMediaInfo(media, articleId){
 
 				media = merge(media, {
 					provider: 'local',
+					thumbnail: suffixFilename('-thumb', media.url),
 					width,
 					height,
 					html,
@@ -26,13 +29,39 @@ export function getLocalMediaInfo(media, articleId){
 
 				media = merge(media, getRatio(width, height));
 
-				return dispatch({
+				dispatch({
 					type: UPDATE_MEDIA,
 					mediaId: media.id,
 					articleId,
 					media,
-				})
+				});
+
+				return media
 			})
+			.then(media => resizeImage(media.url))
 			.catch(error => logger.error('Error parsing media info', error));
 	}
+}
+
+function suffixFilename(sizeName, filename) {
+	return filename.replace(/\.([a-z0-9]+)$/i, function(extension){
+		return sizeName + extension;
+	});
+}
+
+function resizeImage(path) {
+	var sourcePath = join(CONF.pathToBlog, 'data', path);
+
+	return Jimp.read(sourcePath)
+		.then(image => image.cover(
+			CONF.theme.imageSizes.thumb.width,
+			CONF.theme.imageSizes.thumb.height,
+		))
+		.then(image => {
+			logger.info('Writing thumbnail', suffixFilename('-thumb', path));
+
+			return image
+				.quality(80)
+				.write(suffixFilename('-thumb', sourcePath));
+		});
 }
